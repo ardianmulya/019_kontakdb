ad0e402ac2058c84db4539547645b8dc2c9403dd
ardianmulya <ardian.mulya7@gmail.com>
2024-04-28 20:37:55 +0700

    membuat class maps screen

---
+++ lib/screen/maps_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MapsScreen extends StatelessWidget {
+  final Function(String)
+  onLocationSelected;
+  const MapsScreen({super.key, required this.onLocationSelected});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
